3342881e43e2d1ec3b1ef2f7c38b14e7a3938479
John Adams <git@jada.ms>
2022-06-10 12:39:37 -0400

    bash script to start everything

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -euo pipefail
+
+export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
+
+if ! which brew &>/dev/null; then
+    $(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)
+fi
+
+if ! which ansible &>/dev/null; then
+    brew install -q ansible
+fi
+
+ansible-galaxy install -r requirements.yml
+ansible-playbook main.yml
